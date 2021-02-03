66b8aa10ff85c5a98f52fb2a3512e1ce0685bb16
Tan Zhuan Jiang <zjtan1988@gmail.com>
2021-02-04 00:44:15 +0800

    Default Template

---
+++ StringFormulaCalculator/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace StringFormulaCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtStringParameter = new System.Windows.Forms.TextBox();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.lbStringParameter = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 74F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13F));
+            this.tableLayoutPanel1.Controls.Add(this.txtStringParameter, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.rtbResult, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lbStringParameter, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnCalculate, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.70213F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.29787F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 561);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // txtStringParameter
+            // 
+            this.txtStringParameter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtStringParameter.Location = new System.Drawing.Point(104, 22);
+            this.txtStringParameter.Name = "txtStringParameter";
+            this.txtStringParameter.Size = new System.Drawing.Size(574, 20);
+            this.txtStringParameter.TabIndex = 1;
+            this.txtStringParameter.Text = "10 - ( 2 + 3 * ( 7 - 5 ) )";
+            // 
+            // rtbResult
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.rtbResult, 3);
+            this.rtbResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbResult.Location = new System.Drawing.Point(3, 68);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.ReadOnly = true;
+            this.rtbResult.Size = new System.Drawing.Size(778, 490);
+            this.rtbResult.TabIndex = 3;
+            this.rtbResult.Text = "";
+            // 
+            // lbStringParameter
+            // 
+            this.lbStringParameter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbStringParameter.AutoSize = true;
+            this.lbStringParameter.Location = new System.Drawing.Point(3, 26);
+            this.lbStringParameter.Name = "lbStringParameter";
+            this.lbStringParameter.Size = new System.Drawing.Size(95, 13);
+            this.lbStringParameter.TabIndex = 0;
+            this.lbStringParameter.Text = "String Parameter:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCalculate.Location = new System.Drawing.Point(684, 21);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(97, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "String Formula Calculator";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox txtStringParameter;
+        private System.Windows.Forms.RichTextBox rtbResult;
+        private System.Windows.Forms.Label lbStringParameter;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ StringFormulaCalculator/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StringFormulaCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double result = Calculate(txtStringParameter.Text);
+            rtbResult.Text = result.ToString();
+        }
+
+        public static double Calculate(string sum)
+        {
+            // Your code starts here
+            return 0.0;
+        }
+    }
+}
